32ccfa7b7d7dc5b69b30870e15438bc6bb0ed8ea
Mike Amy <mikeamycoder@gmail.com>
2023-04-01 23:27:27 +0700

    Report generic error directly. Allow specification of model using model(name) before requests

--- chatgpt.py
+++ chatgpt.py
@@ -5,33 +5,40 @@ import os
 
 
 def query(query):
+    if query.startswith('Using model '):
+        close_brace = query.indexof(' ', 12)
+        model, query = query[12:close_brace], query[close_brace+1:]
+    else:
+        model = 'gpt-4'
     if 'OPENAI_API_KEY' not in os.environ:
         return "Please set environment variable OPENAI_API_KEY"
     try:
         import openai
     except ImportError:
         return "Please install openai package. E.g. \npip install openai"
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {
-                "role": "system",
-                "content":'. '.join((
-                    "You are an AI emacs assistant",
-                    "Respond to the user by describing or rewriting code",
-                    "If the user's request sounds like a command that emacs"
-                    " can do, generate the emacs-lisp code for that command",
-                ))
-            },
-            {
-                "role": "user",
-                "content": query
-            },
-        ],
-        temperature=0.3,
-    )
-    return response['choices'][0]['message']['content']
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {
+                    "role": "system",
+                    "content":'. '.join((
+                        "You are an AI emacs assistant",
+                        "Respond to the user by describing or rewriting code",
+                        "If the user's request sounds like a command that emacs"
+                        " can do, generate the emacs-lisp code for that command",
+                    ))
+                },
+                {
+                    "role": "user",
+                    "content": query
+                },
+            ],
+            temperature=0.3,
+        )
+        return response['choices'][0]['message']['content']
+    except Exception as exception:
+        return str(exception)
 
 import sys
 
